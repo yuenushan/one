f833b0d0499a8e83675a51171f90fb329dfffd3d
david <425800970@qq.com>
2019-09-13 11:40:09 +0800

    Instrumentation demo

--- boot-graphql-java-tools/src/main/java/com/example/boot/graphql/BootGraphqlApplication.java
+++ boot-graphql-java-tools/src/main/java/com/example/boot/graphql/BootGraphqlApplication.java
@@ -1,13 +1,21 @@
 package com.example.boot.graphql;
 
+import graphql.execution.instrumentation.Instrumentation;
+import graphql.execution.instrumentation.tracing.TracingInstrumentation;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
 @MapperScan("com.example.boot.graphql.mapper")
 public class BootGraphqlApplication {
+
+    @Bean
+    public Instrumentation tracingInstrumentation() {
+        return new TracingInstrumentation();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BootGraphqlApplication.class, args);
