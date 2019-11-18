0c88cf3748945bd4d3b8da5776d951493879b6df
david <425800970@qq.com>
2019-11-18 16:04:32 +0800

    更新接口请求用法

--- boot-with-docker/src/main/java/com/example/bootwithdocker/BootWithDockerApplication.java
+++ boot-with-docker/src/main/java/com/example/bootwithdocker/BootWithDockerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
@@ -21,7 +23,7 @@ public class BootWithDockerApplication {
 	}
 
 	@RequestMapping("/hello")
-	public String hello(@RequestParam(value = "name") String name) {
+	public String hello(@RequestParam(name = "name") String name) {
 		return "hello " + name;
 	}
 
@@ -36,6 +38,16 @@ public class BootWithDockerApplication {
 	@GetMapping("/users/{id}")
 	public User getUser(@PathVariable("id") Integer userId) {
 		return userMap.get(userId);
+	}
+
+	@GetMapping("/users")
+	public List<User> getUsers(UserParams params) {
+		List<Integer> userIds = params.getUserIds();
+		String nameLike = params.getNameLike();
+		return userIds.stream()
+				.map(userId -> userMap.get(userId))
+				.filter(user -> user.getName().contains(nameLike))
+				.collect(Collectors.toList());
 	}
 
 	public static void main(String[] args) {
---
+++ boot-with-docker/src/main/java/com/example/bootwithdocker/UserParams.java
@@ -0,0 +1,24 @@
+package com.example.bootwithdocker;
+
+import java.util.List;
+
+public class UserParams {
+    private List<Integer> userIds;
+    private String nameLike;
+
+    public List<Integer> getUserIds() {
+        return userIds;
+    }
+
+    public void setUserIds(List<Integer> userIds) {
+        this.userIds = userIds;
+    }
+
+    public String getNameLike() {
+        return nameLike;
+    }
+
+    public void setNameLike(String nameLike) {
+        this.nameLike = nameLike;
+    }
+}
